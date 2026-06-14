52b464f49096e48efd7cb38ea059f60a8bfbc32b
Avi Brooks <avibrooks20@gmail.com>
2026-06-14 14:21:22 +0300

    Enable Microsoft Clarity

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-
+import Clarity from '@microsoft/clarity';
 import { Analytics } from '@vercel/analytics/react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
@@ -6,7 +6,7 @@ import './styles/globals.css';
 import './styles/navbar.css';
 import App from './App.jsx';
 
-
+Clarity.init('6uh2f3gv3');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
