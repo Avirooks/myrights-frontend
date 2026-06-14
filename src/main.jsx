dba8fb4bbb0f4aa179627fc3754b8e12ae9c0382
Avi Brooks <avibrooks20@gmail.com>
2026-06-14 14:43:30 +0300

    Install Microsoft Clarity manually

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles/globals.css';
 import './styles/navbar.css';
 import App from './App.jsx';
 
-Clarity.init('6uh2f3gv3');
+Clarity.init('x6uh2f3gv3');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
